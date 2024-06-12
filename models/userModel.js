df192b19585a9a5c673c0ab7a01a0c98ecc928a5
kajal-2011 <kajalrekha888@gmail.com>
2024-06-12 18:52:50 +0600

    user model update

--- models/userModel.js
+++ models/userModel.js
@@ -19,6 +19,12 @@ const UserSchema = new Schema(
         password: {
             type: String,
             required: true
+        },
+        role: {
+            type: String,
+            enum: ["user", "admin"],
+            default: "user",
+            required: true
         }
     },
     { timestamps: true }
